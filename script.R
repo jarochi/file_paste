050ca187f8d172b727b086faaa4f334570ca15a5
jarochi <jaroslaw.chilimoniuk@gmail.com>
2018-08-06 14:50:36 +0200

    21

--- script.R
+++ script.R
@@ -93,22 +93,22 @@ for (i in files_list) {
 }
 
 
-mega_frame <- mega_frame[,-1]
-write.csv2(mega_frame, file = "foci_count_merged.csv", row.names=FALSE)
+foci_count_merged <- mega_frame[,-1]
+# write.csv2(mega_frame, file = "foci_count_merged.csv", row.names=FALSE)
 
-mega_frame_FITC <- mega_frame_FITC[,-1]
-write.csv2(mega_frame_FITC, file = "foci_count_FITC.csv", row.names=FALSE)
+foci_count_FITC <- mega_frame_FITC[,-1]
+# write.csv2(mega_frame_FITC, file = "foci_count_FITC.csv", row.names=FALSE)
 
-mega_frame_APC <- mega_frame_APC[,-1]
-write.csv2(mega_frame_APC, file = "foci_count_APC.csv", row.names=FALSE)
+foci_count_APC <- mega_frame_APC[,-1]
+# write.csv2(mega_frame_APC, file = "foci_count_APC.csv", row.names=FALSE)
 
 # foci_count_APC <- read.csv2("foci_count_APC.csv")
-save(mega_frame_APC, file = "foci_count_APC.rda")
+save(foci_count_APC, file = "foci_count_APC.rda")
 
 # foci_count_APC <- read.csv2("foci_count_FITC.csv")
-save(mega_frame_FITC, file = "foci_count_FITC.rda")
+save(foci_count_FITC, file = "foci_count_FITC.rda")
 
 
 # foci_count_APC <- read.csv2("foci_count_merged.csv")
-save(mega_frame, file = "foci_count_merged.rda")
+save(foci_count_merged, file = "foci_count_merged.rda")
 
